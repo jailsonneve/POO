a3cf0246cb062817c141f9dca166b6ff30d2f2df
Seu nome de usuário <seuemail@example.com>
2024-03-20 08:26:58 -0300

    Atv04

--- Atividades/Atv01/Atv01.java
+++ Atividades/Atv01/Atv01.java
@@ -1,4 +1,4 @@
-package POO;
+package Atv01;
 public class Atv01 {
     public static void main(String[] args) {
         System.out.println("Disciplina POO");
---
+++ Atividades/Atv04/Atv04.java
@@ -0,0 +1,6 @@
+package Atv04;
+public class Atv04 {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaa");
+    }
+}
